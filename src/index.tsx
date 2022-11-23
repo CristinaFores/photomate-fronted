b044499fdd64c6616d118ba199a40d6e9487a211
cristinafores <cristinaforescampos1992@gmail.com>
2022-11-23 18:03:23 +0100

    Basic css normalize

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import App from "./components/App/App";
 import { store } from "./redux/store";
 import reportWebVitals from "./reportWebVitals";
+import GlobalStyle from "./style/GlobalStyle";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -11,6 +12,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <GlobalStyle />
       <App />
     </Provider>
   </React.StrictMode>
