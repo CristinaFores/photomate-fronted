a47418119ec82ffa2c3031f8abdfe33efaacaa59
cristinafores <cristinaforescampos1992@gmail.com>
2022-11-23 13:08:19 +0100

    Test slice showModalError

--- src/redux/features/uiSlice/uiSlice.test.ts
+++ src/redux/features/uiSlice/uiSlice.test.ts
@@ -1,26 +1,71 @@
 import { UiState } from "./types";
-import { setLoadinTrueActionCreator, uiReducer } from "./uiSlice";
+import {
+  setLoadingFalseActionCreator,
+  setLoadingTrueActionCreator,
+  showModalActionCreator,
+  uiReducer,
+} from "./uiSlice";
 
 const mockUiState: UiState = {
   modal: {
-    textmodal: "",
+    text: "",
     showModal: false,
-    error: false,
+    isError: false,
   },
   isLoading: false,
 };
 
-describe("Given a uiReducer", () => {
-  describe("When it is invoked it receives the initial ui state mockUiState", () => {
-    test("Then it should return a copy of the initial state with setLoadingTrue", () => {
+describe("Given a uiReducer setLoadingTrue", () => {
+  describe("When it is invoked it receives the initial ui state mockUiState with isLoading", () => {
+    test("Then it should return a copy of the initial state with setLoadingTrue with isLoading: true", () => {
       const expectedUiState = {
         ...mockUiState,
         isLoading: true,
       };
 
-      const newUiState = uiReducer(mockUiState, setLoadinTrueActionCreator());
+      const newUiState = uiReducer(mockUiState, setLoadingTrueActionCreator());
 
       expect(newUiState).toStrictEqual(expectedUiState);
+    });
+  });
+});
+
+describe("Given a uiReducer setLoadingFalse", () => {
+  describe("When it is invoked it receives the  the initial ui state mockUiState with isLoading", () => {
+    test("Then it shouldreturn a copy of the initial state with setoadinfFalse with isLoading: false", () => {
+      const expectedUiState = {
+        ...mockUiState,
+        isLoading: false,
+      };
+      const newUiState = uiReducer(mockUiState, setLoadingFalseActionCreator());
+
+      expect(newUiState).toStrictEqual(expectedUiState);
+    });
+  });
+});
+
+describe("Given a uiReducer showModalError", () => {
+  describe("When it is invoked it receives the  the initial ui state mockUiState", () => {
+    test("Then it shouldreturn a copy of the initial state with showModalError with showModal: false", () => {
+      const expectedUiState = {
+        modal: {
+          isError: true,
+          text: "This is error",
+          showModal: true,
+        },
+        isLoading: false,
+      };
+      const expextActionPayload = {
+        isError: true,
+        text: "This is error",
+      };
+
+      const newUiState = uiReducer(
+        mockUiState,
+        showModalActionCreator(expextActionPayload)
+      );
+
+      expect(newUiState.modal).toStrictEqual(expectedUiState.modal);
     });
   });
 });
