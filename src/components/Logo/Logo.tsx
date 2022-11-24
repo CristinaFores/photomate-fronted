09f5b60b84d060378cb4944d18a2b41bb2fb9936
cristinafores <cristinaforescampos1992@gmail.com>
2022-11-24 22:59:38 +0100

    Create component Logo and customize styled

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,7 @@
+import { LogoStyled } from "./LogoStyled";
+
+const Logo = (): JSX.Element => {
+  return <LogoStyled>Photomate</LogoStyled>;
+};
+
+export default Logo;
---
+++ src/components/Logo/LogoStyled.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import mainStyleColors from "../../style/themeColors";
+
+export const LogoStyled = styled.h1`
+  font-family: "Dancing Script";
+  color: ${mainStyleColors.primaryColor};
+  font-weight: 2.5rem;
+  font-family: Dancing Script;
+  font-weight: bold;
+`;
