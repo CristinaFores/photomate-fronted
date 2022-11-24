b112a41452281619aacdde58b1381a90f0fcf638
cristinafores <cristinaforescampos1992@gmail.com>
2022-11-24 22:51:42 +0100

    Add test component Loading

---
+++ src/components/Loading/Loading.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import Loading from "./Loading";
+
+describe("Given a Loading component", () => {
+  describe("When it render", () => {
+    test("Then it should reply with the message 'Please wait' by the accesibility function'", () => {
+      const expectedMessage = "Please wait";
+      render(<Loading />);
+
+      const message = screen.getByTestId("custom-element");
+
+      expect(message).toHaveAttribute("aria-label", expectedMessage);
+    });
+  });
+});
