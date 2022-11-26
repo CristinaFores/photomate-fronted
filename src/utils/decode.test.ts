79629935937e79f50030b405708de5e956a53e7d
cristinafores <cristinaforescampos1992@gmail.com>
2022-11-26 17:31:38 +0100

    Fix test decode function

--- src/utils/decode.test.ts
+++ src/utils/decode.test.ts
@@ -1,16 +1,27 @@
-import { JwtPayloadCustom } from "./types";
 import jwtDecode from "jwt-decode";
+import decodeToken from "./decode";
+import { JwtPayloadCustom } from "./types";
 
 describe("Given a decodeToken funcion", () => {
   describe("When it's called with a token encode", () => {
-    test("Then it should  un objecto son el token descodificado , la id, y el nombre del usuario", () => {
-      const token =
+    test("Then it should  return objecto son el token , la id, and username", () => {
+      const expectoken =
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImNyaXN0aW5hIiwiaWQiOiIxMjM0NTY3ODkiLCJpYXQiOjE2NjgyNjkxNzksImV4cCI6MTY2ODQ0MTk3OX0.SscRMw9fHjIbNR-cd9NjzCxz-AUUpJ1YSQjZghZOcwo";
+
+      const { id, token, username } = decodeToken(expectoken);
+
+      const tokenexpect = {
+        id: "123456789",
+        token:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImNyaXN0aW5hIiwiaWQiOiIxMjM0NTY3ODkiLCJpYXQiOjE2NjgyNjkxNzksImV4cCI6MTY2ODQ0MTk3OX0.SscRMw9fHjIbNR-cd9NjzCxz-AUUpJ1YSQjZghZOcwo",
+        username: "cristina",
+      };
 
       const jwtPayload: JwtPayloadCustom = jwtDecode(token);
 
       const expectedId = "123456789";
 
+      expect({ token, id, username }).toStrictEqual(tokenexpect);
       expect(jwtPayload.id).toStrictEqual(expectedId);
     });
   });
