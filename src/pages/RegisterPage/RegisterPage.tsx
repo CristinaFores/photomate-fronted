f274457381bd59b450ebf0c0f454eac53f8bef7f
cristinafores <cristinaforescampos1992@gmail.com>
2022-11-27 19:01:15 +0100

    Fix styles registerpage, register component

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -1,15 +1,15 @@
 import Header from "../../components/Header/Header";
 import Logo from "../../components/Logo/Logo";
 import Register from "../../components/Register/Register";
-import { RegisterPageStyled } from "./RegisterPageStyled";
+import { FormPageStyled } from "./RegisterPageStyled";
 
 const RegisterPage = (): JSX.Element => {
   return (
     <>
       <Header children={<Logo />} />
-      <RegisterPageStyled>
+      <FormPageStyled>
         <Register />
-      </RegisterPageStyled>
+      </FormPageStyled>
     </>
   );
 };
