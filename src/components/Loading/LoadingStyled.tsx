c086c2862082f4704616e088db500a4105e1a7d7
cristinafores <cristinaforescampos1992@gmail.com>
2022-11-28 16:36:05 +0100

    Customize styles center pages

--- src/components/Loading/LoadingStyled.tsx
+++ src/components/Loading/LoadingStyled.tsx
@@ -1,6 +1,19 @@
 import styled from "styled-components";
 
 export const LoadingStyled = styled.span`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  height: 100%;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 1;
+  background-color: rgb(82 131 221 / 50%);
+
   .loader {
     width: 48px;
     height: 48px;
@@ -10,14 +23,15 @@ export const LoadingStyled = styled.span`
   .loader::after,
   .loader::before {
     content: "";
+    content: "";
     box-sizing: border-box;
     width: 80px;
     height: 80px;
     border-radius: 50%;
-    border: 2px solid ${(props) => props.theme.colors.primary.dark};
+    border: 8px solid #0024ff;
     position: absolute;
     left: 0;
-    top: 0;
+    top: -85px;
     animation: animloader 2s linear infinite;
   }
   .loader::after {
