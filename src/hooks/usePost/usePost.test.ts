80b9fdcb34a6f41b7c658db7baebb2525cddb0ce
cristinafores <cristinaforescampos1992@gmail.com>
2022-11-29 21:38:18 +0100

    Add test custom hook post

---
+++ src/hooks/usePost/usePost.test.ts
@@ -0,0 +1,34 @@
+import { renderHook } from "@testing-library/react";
+import axios from "axios";
+import ProviderWrapper from "../../mocks/ProwiderWrapper";
+import { mockInitialStore } from "../../mocks/storeMock";
+
+import { showLoadingActionCreator } from "../../redux/features/uiSlice/uiSlice";
+import usePost from "./usePost";
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+const dispatchSpy = jest.spyOn(mockInitialStore, "dispatch");
+
+describe("Given the custom hook usePost", () => {
+  describe("When it's methodt loadPost is invoked", () => {
+    test("Then dispatch should be called error with showModalActionCreator", async () => {
+      const {
+        result: {
+          current: { loadPosts },
+        },
+      } = renderHook(() => usePost(), {
+        wrapper: ProviderWrapper,
+      });
+
+      jest.mock("axios");
+      const mockedAxios = axios as jest.Mocked<typeof axios>;
+      mockedAxios.get.mockResolvedValueOnce(undefined);
+      await loadPosts();
+
+      expect(dispatchSpy).toHaveBeenCalledWith(showLoadingActionCreator());
+    });
+  });
+});
