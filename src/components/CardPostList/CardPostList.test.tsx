d6cf3687bf4c637a232a2aa7599d1024b3f7a423
cristinafores <cristinaforescampos1992@gmail.com>
2022-11-30 15:18:58 +0100

    Test cardpostlist

--- src/components/CardPostList/CardPostList.test.tsx
+++ src/components/CardPostList/CardPostList.test.tsx
@@ -4,7 +4,7 @@ import CardPostList from "./CardPostList";
 
 describe("Given component CardPostList", () => {
   describe("When it render", () => {
-    test("Then its should show card with titles:'Cristina', 'Que guay el bootcamp' and button 'Leer más'", () => {
+    test("Then its should show card with titles:'Cristina' and button 'Leer más'", () => {
       renderWithProviders(<CardPostList />);
 
       const titleUser = screen.getByRole("heading", {
