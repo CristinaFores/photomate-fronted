714cd318c6a6e670193ec6311f399a3ad4476ef0
cristinafores <cristinaforescampos1992@gmail.com>
2022-11-30 15:44:35 +0100

    Add  getToken in app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 
 import useToken from "../../hooks/useToken/useToken";
 import { useAppSelector } from "../../redux/hooks";
+
 import Layout from "../Layout/Layout";
 import Loading from "../Loading/Loading";
 import Modal from "../Modal/Modal";
@@ -14,7 +15,9 @@ const App = () => {
 
   const { getToken } = useToken();
 
-  useEffect(() => {}, [getToken]);
+  useEffect(() => {
+    getToken();
+  }, [getToken]);
 
   return (
     <>
