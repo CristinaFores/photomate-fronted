3dc6a5242430b6d1417e91e6661eae8ca3712b65
cristinafores <cristinaforescampos1992@gmail.com>
2022-11-30 23:14:49 +0100

    Test list component

--- src/components/ListPostsHome/ListPostsHome.test.tsx
+++ src/components/ListPostsHome/ListPostsHome.test.tsx
@@ -1,19 +1,14 @@
 import renderWithProviders from "../../mocks/storeMock";
 import { screen } from "@testing-library/react";
 import ListPostsHome from "./ListPostsHome";
+import CardPostList from "../CardPostList/CardPostList";
 
 describe("Given component ListPostsHome", () => {
   describe("When it render", () => {
-    test("Then its should show card with titles:'Cristina' and list cards '", () => {
+    test("Then its should show card with headding and list cards '", () => {
       renderWithProviders(<ListPostsHome />);
 
-      const titleUser = screen.queryByRole("heading", {
-        name: "Cristina",
-      });
-
       const listPost = screen.getByRole("list");
-
-      expect(titleUser).toBeInTheDocument();
 
       expect(listPost).toBeInTheDocument();
     });
