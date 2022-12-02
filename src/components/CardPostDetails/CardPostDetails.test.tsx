8339fdbdffaa045ed26a08199e2c3c9c39ddd00e
cristinafores <cristinaforescampos1992@gmail.com>
2022-12-02 20:36:29 +0100

    Add test CardDetailPost , case useNavigate

--- src/components/CardPostDetails/CardPostDetails.test.tsx
+++ src/components/CardPostDetails/CardPostDetails.test.tsx
@@ -1,19 +1,30 @@
 import { screen } from "@testing-library/react";
 import renderWithProviders from "../../mocks/storeMock";
 import CardDetailPost from "./CardPostDetails";
+import * as router from "react-router";
+import userEvent from "@testing-library/user-event";
+
+const navigate = jest.fn();
+
+beforeEach(() => {
+  jest.spyOn(router, "useNavigate").mockImplementation(() => navigate);
+});
 
 describe("Given component CardDetailPost", () => {
   describe("When it render", () => {
-    test("Then its should show card with img with text 'imagen de la publicacion', one button  ,", () => {
+    test("Then its should show one button and call navigate", async () => {
       renderWithProviders(
-        <CardDetailPost description="descripcion" owner="cris" title="hola" />
+        <CardDetailPost
+          description="descripcion"
+          owner="cris"
+          title="hola"
+          imagePaths={["", ""]}
+        />
       );
+      const button = screen.getByRole("button");
+      await userEvent.click(button);
 
-      const titleUser = screen.queryByRole("img", {
-        name: "imagen de la publicacion",
-      });
-
-      expect(titleUser).toBeInTheDocument();
+      expect(navigate).toHaveBeenCalledWith("/home");
     });
   });
 });
