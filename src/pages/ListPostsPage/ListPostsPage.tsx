8ad20a8ff497eb48b7114608d20cb019c4e83595
cristinafores <cristinaforescampos1992@gmail.com>
2022-12-02 21:27:08 +0100

    Add page List

---
+++ src/pages/ListPostsPage/ListPostsPage.tsx
@@ -0,0 +1,11 @@
+import ListPosts from "../../components/ListPosts/ListPosts";
+
+const ListPostPage = (): JSX.Element => {
+  return (
+    <>
+      <ListPosts />
+    </>
+  );
+};
+
+export default ListPostPage;
