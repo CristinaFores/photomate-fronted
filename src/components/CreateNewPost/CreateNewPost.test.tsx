5a61a6b55a8b8374a868dbcb7bab8d2dffef31c7
cristinafores <cristinaforescampos1992@gmail.com>
2022-12-06 17:56:09 +0100

    Add test CreateNewPost

--- src/components/CreateNewPost/CreateNewPost.test.tsx
+++ src/components/CreateNewPost/CreateNewPost.test.tsx
@@ -17,11 +17,43 @@ describe("Given component CreateNewPost", () => {
       const expectButton = screen.getByRole("button", {
         name: "Publicar",
       });
+      const file = new File(["hello"], "hello.png", { type: "image/png" });
+
+      const imageUpload = screen.getByLabelText(/imagen/i);
 
       await userEvent.type(expectTextBox, "hola");
       await userEvent.type(expectTextBoxDescription, "hola");
-
+      await userEvent.upload(imageUpload, file);
       await userEvent.click(expectButton);
+
+      expect(expectTextBox).toBeInTheDocument();
+      expect(expectTextBoxDescription).toBeInTheDocument();
+      expect(expectButton).toBeInTheDocument();
+    });
+  });
+
+  describe("When it render CreateNewPost", () => {
+    test("Then its should show button with text 'Modificar', and textbox with text: 'Titulo de la la publicacion*'", async () => {
+      renderWithProviders(<CreateNewPost isEditMode={true} />);
+
+      const expectTextBox = screen.getByRole("textbox", {
+        name: "Titulo de la la publicacion*",
+      });
+      const expectTextBoxDescription = screen.getByRole("textbox", {
+        name: "Texto de la publicacion",
+      });
+      const expectButton = screen.getByRole("button", {
+        name: "Modificar",
+      });
+      const file = new File(["hello"], "hello.png", { type: "image/png" });
+
+      const imageUpload = screen.getByLabelText(/imagen/i);
+
+      await userEvent.type(expectTextBox, "hola");
+      await userEvent.type(expectTextBoxDescription, "hola");
+      await userEvent.upload(imageUpload, file);
+      await userEvent.click(expectButton);
+
       expect(expectTextBox).toBeInTheDocument();
       expect(expectTextBoxDescription).toBeInTheDocument();
       expect(expectButton).toBeInTheDocument();
