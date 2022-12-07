c87d2d3350285173256a012673fc62b6c31a3634
cristinafores <cristinaforescampos1992@gmail.com>
2022-12-07 01:42:14 +0100

    Add link in component logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
 import { LogoStyled } from "./LogoStyled";
 
 const Logo = (): JSX.Element => {
-  return <LogoStyled>Photomate</LogoStyled>;
+  return (
+    <Link to={"/home"}>
+      <LogoStyled>Photomate</LogoStyled>
+    </Link>
+  );
 };
 
 export default Logo;
