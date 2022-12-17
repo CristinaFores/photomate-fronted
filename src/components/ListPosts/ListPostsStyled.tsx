95d06d0a3459af639bcd72da82a1231118b97a10
Cristina <cristinaforescampos1992@gmail.com>
2022-12-17 21:07:37 +0100

    Add styles card

--- src/components/ListPosts/ListPostsStyled.tsx
+++ src/components/ListPosts/ListPostsStyled.tsx
@@ -3,7 +3,21 @@ import styled from "styled-components";
 export const ListPostsStyled = styled.ul`
   display: flex;
   flex-direction: column;
-  gap: 1rem;
+  gap: 3rem;
+
+  @media (min-width: 600px) {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-auto-columns: 1fr;
+    grid-auto-rows: 1fr;
+  }
+
+  @media (min-width: 900px) {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-auto-columns: 1fr;
+    grid-auto-rows: 1fr;
+  }
 `;
 
 export const NoResultsStyled = styled.p`
